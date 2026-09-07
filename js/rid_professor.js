368f98b43e8dd828d444128e1db7d32369f70e30
anton <santana.cin@gmail.com>
2026-09-07 08:17:30 -0300

    Versão de carregarComponente que funciona no PID mas não no RID.

--- js/rid_professor.js
+++ js/rid_professor.js
@@ -97,20 +97,6 @@ function atualizar_horas_executadas(id_atividade_docente, id_tipo_atividade) {
     }
 }
 
-//function carregarComponente(metodo, id) {
-//    $('#metodo').val(metodo);
-//    var dados = $('#formulario').serialize();
-//    $.ajax({
-//        url: 'controller/' + classe + '.php',
-//        type: 'post',
-//        dataType: 'html',
-//        data: dados
-//    }).done(function (resposta) {
-//        var json = JSON.parse(resposta);
-//        $('#' + id).html(json.select);
-//    });
-//}
-
 function carregarComponente(metodo, id) {
     var r = $.Deferred();
     $('#metodo').val(metodo);
